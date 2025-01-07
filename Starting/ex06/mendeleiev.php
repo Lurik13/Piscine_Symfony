d88be8ffc690d23fa6d9d2eb40102ba9a8889fbb
Lucas Ribette <116387708+Lurik13@users.noreply.github.com>
2025-01-07 11:37:49 +0100

    Now it creates mendeleiev.html

--- Starting/ex06/mendeleiev.php
+++ Starting/ex06/mendeleiev.php
@@ -1,4 +1,8 @@
 <?php 
+    /* *************************************************************** */
+    /* *************************** BACKEND *************************** */
+    /* *************************************************************** */
+
     class Element
     {
         public $name;
@@ -45,20 +49,23 @@
             array_push($elements, $new_elem);
             $line = fgets($file);
         }
-        // var_dump($elements);
         return $elements;
     }
 
-    $file = fopen("ex06.txt","r");
-    $elements = fill_elements_array($file);
-    fclose($file);
-?>
 
+
+    /* *************************************************************** */
+    /* ************************** FRONTEND *************************** */
+    /* *************************************************************** */
+
+    function head($html)
+    {
+        fwrite($html, "
 <!DOCTYPE html>
 <html>
     <head>
         <title>Mendeleiev</title>
-        <meta charset="UTF-8">
+        <meta charset=\"UTF-8\">
         <style>
             td {
                 border: 1px solid black;
@@ -68,50 +75,102 @@
                 border: 0px;
             }
         </style>
-    </head>
-    <body style="background-color:#1e1f22;">
-        <table style="color:white">
-            <!-- Skip toutes les cases vides -->
-            <?php $i = 0; ?>
-            <?php foreach ($elements as $element): ?>
-                <?php if ($element->position == 0): ?>
-                    <?php $i = 0 ?>
-                    <tr>
-                <?php endif; ?>
-                <?php while ($element->position != $i):?>
-                    <td class="void"></td>
-                    <?php $i++ ?>
-                <?php endwhile;?>
-                <?php $i++ ?>
+    </head>");
+    }
 
-                <!-- Pour les cases pleines -->
-                <?php if ($element->position == 0 && $element->number != 1): ?> 
-                    <td style="background-color:#224354">
-                <?php elseif ($element->position == 1): ?> 
-                    <td style="background-color:#4b372e">
-                <?php elseif ($element->position == 17): ?> 
-                    <td style="background-color:#3f3370">
-                <?php elseif ($element->position >= 2 && $element->position <= 11 && ($element->number < 109 || $element->number > 117)): ?> 
-                    <td style="background-color:#2c4425">
-                <?php elseif ($element->number >= 109 && $element->number <= 117): ?> 
-                    <td style="background-color:#46474c">
-                <?php elseif (in_array($element->number, [5, 14, 32, 33, 51, 52])): ?> 
-                    <td style="background-color:#612339">
-                <?php elseif (in_array($element->number, [13, 31, 49, 50, 81, 82, 83, 84, 85])): ?> 
-                    <td style="background-color:#664915">
-                <?php else : ?>
-                    <td style="background-color:#6b2b00">
-                <?php endif; ?> 
-                    <h4><?php echo $element->name ?></h4>
+    function skip_empty_slots($html, $element, $i)
+    {
+        if ($element->position == 0)
+        {
+            $i = 0;
+            fwrite($html, "
+            <tr>");
+        }
+        while ($element->position != $i)
+        {
+            fwrite($html, "
+                <td class=\"void\"></td>");
+            $i++;
+        }
+        return ++$i;
+    }
+
+    function color_full_cells($html, $element)
+    {
+        fwrite($html, "
+                <td style=\"background-color:");
+        if ($element->position == 0 && $element->number != 1)
+            fwrite($html, "#224354\">");
+        elseif ($element->position == 1)
+            fwrite($html, "#4b372e\">");
+        elseif ($element->position == 17)
+            fwrite($html, "#3f3370\">");
+        elseif ($element->position >= 2 && $element->position <= 11 && ($element->number < 109 || $element->number > 117))
+            fwrite($html, "#2c4425\">");
+        elseif ($element->number >= 109 && $element->number <= 117)
+            fwrite($html, "#46474c\">");
+        elseif (in_array($element->number, [5, 14, 32, 33, 51, 52]))
+            fwrite($html, "#612339\">");
+        elseif (in_array($element->number, [13, 31, 49, 50, 81, 82, 83, 84, 85]))
+            fwrite($html, "#664915\">");
+        else
+            fwrite($html, "#6b2b00\">");
+    }
+
+    function write_in_cells($html, $element)
+    {
+        fwrite($html, "
+                    <h4>$element->name</h4>
                     <ul>
-                        <li>Nb <?php echo $element->number ?></li>
-                        <li><?php echo $element->small ?></li>
-                        <li> <?php echo $element->molar ?> </li>
-                        <li><?php echo $element->nb_electrons ?> electron<?php 
-                            if ($element->nb_electrons > 1): echo 's'; endif?></li>
+                        <li>Nb $element->number</li>
+                        <li>$element->small</li>
+                        <li>$element->molar</li>
+                        <li>$element->nb_electrons electron"); 
+        if ($element->nb_electrons > 1)
+            fwrite($html, "s");
+        fwrite($html, "
+                        </li>
                     </ul>
-                </td>
-            <?php endforeach;?>
+                </td>");
+    }
+
+    function body($html, $elements)
+    {
+        fwrite($html, "
+    <body style=\"background-color:#1e1f22;\">
+        <table style=\"color:white\">");
+        $i = 0;
+        foreach ($elements as $element)
+        {
+            $i = skip_empty_slots($html, $element, $i);
+            color_full_cells($html, $element);
+            write_in_cells($html, $element);
+        }
+        fwrite($html,"
         </table>
     </body>
-</html>
+</html>");
+    }
+
+
+
+    /* *************************************************************** */
+    /* ***************************** MAIN **************************** */
+    /* *************************************************************** */
+    
+    function mendeleiev()
+    {
+        $file = fopen("ex06.txt","r");
+        $elements = fill_elements_array($file);
+        fclose($file);
+
+        if (file_exists("mendeleiev.html"))
+            unlink('mendeleiev.html');
+        $html = fopen("mendeleiev.html", "a");
+        head($html);
+        body($html, $elements);
+        fclose($html);
+    }
+
+    mendeleiev();
+?>
